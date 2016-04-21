5674af631284823109b7fef143522a75341a3627
nkayastha <nkayastha@veriskhealth.com>
2016-04-21 23:30:21 +0545

    Exploratory Data Analysis Plotting Assignment 1

---
+++ plot3.R
@@ -0,0 +1,25 @@
+## Exploratory Data Analysis Plotting Assignment 1
+## Generates plot3.png by running teh plot3.R
+
+File <- "./household_power_consumption.txt"
+data <- read.table(File, 
+                   header=TRUE, 
+                   sep=";", 
+                   stringsAsFactors=FALSE, 
+                   dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+
+## Creating plot3.pnd
+png("plot3.png", width=480, height=480)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
